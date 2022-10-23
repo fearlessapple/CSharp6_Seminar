e628d9fa8356d77350e2b3c1101f6f3e105838c9
Stas <smaster950@gmail.com>
2022-10-23 20:46:34 +0300

    Complete Exercise41

--- Exercise41/Program.cs
+++ Exercise41/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите наименьшее значение массива: ");
+int minValue = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите наибольшее значение массива: ");
+int maxValue = int.Parse(Console.ReadLine() ?? "");
+
+
+int[] array = GetArray(size, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+FindInArray(array);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FindInArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum++;
+    }
+    Console.WriteLine($"Кол-во чисел больше 0: {sum}");
+}
