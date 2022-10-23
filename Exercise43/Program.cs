7911df173a2fd4ca71dd004b388fa3ffe01c847d
Stas <smaster950@gmail.com>
2022-10-23 21:26:52 +0300

    Complete Exercise43

--- Exercise43/Program.cs
+++ Exercise43/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine() ?? "");
+double b11 = (double)b1;
+
+Console.Write("Введите  значение k1: ");
+int k1 = int.Parse(Console.ReadLine() ?? "");
+double k11 = (double)k1;
+
+Console.Write("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine() ?? "");
+double b22 = (double)b2;
+
+Console.Write("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine() ?? "");
+double k22 = (double)k2;
+
+double x = (b22 - b11) / (k11 - k22);
+double y1 = k11 * x + b11;
+double y2 = k22 * x + b22;
+double Delta = Math.Abs(y1 - y2);
+
+if (Delta < 0.00001)
+{
+    Console.WriteLine($"Точка пересечения прямых: ({x}; {y1})");
+}
+else
+{
+    Console.WriteLine("Точек пересечения нет.");
+}
